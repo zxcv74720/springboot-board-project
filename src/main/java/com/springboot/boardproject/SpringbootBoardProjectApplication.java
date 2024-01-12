5f45ea75a5b0cb184df5ad2c63c270f8b7f9b600
zxcv74720 <zxcv74720@naver.com>
2024-01-12 19:01:29 +0900

    #4 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술은:
    
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/springboot/boardproject/SpringbootBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.boardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBoardProjectApplication.class, args);
+    }
+
+}
