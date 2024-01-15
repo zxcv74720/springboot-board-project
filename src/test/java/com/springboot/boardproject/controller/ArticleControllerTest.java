91b00397caed71bf02d98464ac9caa25e6592381
zxcv74720 <zxcv74720@naver.com>
2024-01-15 21:18:00 +0900

    #14 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 31c475 에서 확인했기에 나머지 뷰도 동일하게 작동할 것으로 예상하고 미리 테스트 메서드를 업데이트함

--- src/test/java/com/springboot/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/springboot/boardproject/controller/ArticleControllerTest.java
@@ -44,7 +44,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("articles"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -59,7 +59,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search"));
     }
 
@@ -72,7 +72,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(model().attributeExists("articles/search-hashtag"));
     }
 }
