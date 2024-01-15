31c4752c082479bc70a482f22c31c8588ba3f98e
zxcv74720 <zxcv74720@naver.com>
2024-01-15 20:55:17 +0900

    #14 - 게시판 뷰 기본 골격 작성
    컨트롤러가 핸들러 메서드로 테스트를 통과하게 열어주고, 뷰를 적절히 뼈대 잡음
    
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기에 적절히 테스트 메서드를 수정함

--- src/main/java/com/springboot/boardproject/controller/ArticleController.java
+++ src/main/java/com/springboot/boardproject/controller/ArticleController.java
@@ -1,7 +1,11 @@
 package com.springboot.boardproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * /articles
@@ -12,5 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles", List.of());
+        return "articles/index";
+    }
 
 }
--- src/test/java/com/springboot/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/springboot/boardproject/controller/ArticleControllerTest.java
@@ -22,7 +22,6 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
@@ -31,7 +30,7 @@ class ArticleControllerTest {
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
