ef033a75d8ec8282a9047f00f895f5f8524f9dd4
zxcv74720 <zxcv74720@naver.com>
2024-01-17 20:29:43 +0900

    #14 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용함

--- src/test/java/com/springboot/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/springboot/boardproject/controller/ArticleControllerTest.java
@@ -35,7 +35,6 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"));
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
@@ -46,7 +45,7 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
-                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
     }
 
