febdba9ae5c69d7d7144b116f4323543ab6f033f
zxcv74720 <zxcv74720@naver.com>
2024-01-22 22:42:53 +0900

    #39 - 게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현

--- src/main/java/com/springboot/boardproject/service/ArticleService.java
+++ src/main/java/com/springboot/boardproject/service/ArticleService.java
@@ -1,11 +1,12 @@
 package com.springboot.boardproject.service;
 
 import com.springboot.boardproject.domain.Article;
-import com.springboot.boardproject.domain.type.SearchType;
+import com.springboot.boardproject.domain.UserAccount;
+import com.springboot.boardproject.domain.constant.SearchType;
 import com.springboot.boardproject.dto.ArticleDto;
-import com.springboot.boardproject.dto.ArticleUpdateDto;
 import com.springboot.boardproject.dto.ArticleWithCommentsDto;
 import com.springboot.boardproject.repository.ArticleRepository;
+import com.springboot.boardproject.repository.UserAccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,13 +24,13 @@ import java.util.List;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserAccountRepository userAccountRepository;
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         if (searchKeyword == null || searchKeyword.isBlank()) {
             return articleRepository.findAll(pageable).map(ArticleDto::from);
         }
-
         return switch (searchType) {
             case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
             case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
@@ -40,44 +41,47 @@ public class ArticleService {
     }
 
     @Transactional(readOnly = true)
-    public ArticleWithCommentsDto getArticle(Long articleId) {
+    public ArticleWithCommentsDto getArticleWithComments(Long articleId) {
         return articleRepository.findById(articleId)
                 .map(ArticleWithCommentsDto::from)
                 .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
-    public void saveArticle(ArticleDto dto) {
-        articleRepository.save(dto.toEntity());
+    @Transactional(readOnly = true)
+    public ArticleDto getArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .map(ArticleDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
-    public void updateArticle(ArticleDto dto) {
+    public void saveArticle(ArticleDto dto) {
+        UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+        articleRepository.save(dto.toEntity(userAccount));
+    }
+
+    public void updateArticle(Long articleId, ArticleDto dto) {
         try {
-            Article article = articleRepository.getReferenceById(dto.id());
+            Article article = articleRepository.getReferenceById(articleId);
             if (dto.title() != null) { article.setTitle(dto.title()); }
             if (dto.content() != null) { article.setContent(dto.content()); }
             article.setHashtag(dto.hashtag());
         } catch (EntityNotFoundException e) {
-            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다. - dto: {}", dto);
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
         }
     }
-
     public void deleteArticle(long articleId) {
         articleRepository.deleteById(articleId);
     }
-
     public long getArticleCount() {
         return articleRepository.count();
     }
-
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
         if (hashtag == null || hashtag.isBlank()) {
             return Page.empty(pageable);
         }
-
         return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
     }
-
     public List<String> getHashtags() {
         return articleRepository.findAllDistinctHashtags();
     }
