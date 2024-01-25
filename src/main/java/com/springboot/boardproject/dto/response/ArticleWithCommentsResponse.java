a674be83fbb94cc76be9bd9b90fd9be14581aefd
zxcv74720 <zxcv74720@naver.com>
2024-01-25 11:07:26 +0900

    #47 - 수정, 삭제를 할 때 해당 사용자가 맞는지 확인하기 위한 응답 정보 추가
    인증 정보의 `userId`와
    해당 게시글/댓글의 작성자 `userId`가 동일한지 알려면
    응답에서 `userId`를 내려줘야 함
    이를 response dto 에 반영

--- src/main/java/com/springboot/boardproject/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/springboot/boardproject/dto/response/ArticleWithCommentsResponse.java
@@ -16,11 +16,12 @@ public record ArticleWithCommentsResponse(
         LocalDateTime createdAt,
         String email,
         String nickname,
-        Set<ArticleCommentResponse> articleCommentResponse
+        String userId,
+        Set<ArticleCommentResponse> articleCommentsResponse
 ) {
 
-    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, Set<ArticleCommentResponse> articleCommentResponses) {
-        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, articleCommentResponses);
+    public static ArticleWithCommentsResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname, String userId, Set<ArticleCommentResponse> articleCommentResponses) {
+        return new ArticleWithCommentsResponse(id, title, content, hashtag, createdAt, email, nickname, userId, articleCommentResponses);
     }
 
     public static ArticleWithCommentsResponse from(ArticleWithCommentsDto dto) {
@@ -28,7 +29,6 @@ public record ArticleWithCommentsResponse(
         if (nickname == null || nickname.isBlank()) {
             nickname = dto.userAccountDto().userId();
         }
-
         return new ArticleWithCommentsResponse(
                 dto.id(),
                 dto.title(),
@@ -37,6 +37,7 @@ public record ArticleWithCommentsResponse(
                 dto.createdAt(),
                 dto.userAccountDto().email(),
                 nickname,
+                dto.userAccountDto().userId(),
                 dto.articleCommentDtos().stream()
                         .map(ArticleCommentResponse::from)
                         .collect(Collectors.toCollection(LinkedHashSet::new))
